2f7a90e82f21782e8e82651409c7ec3339e8d08c
jsgilberto <alv.mtz94@gmail.com>
2018-05-26 00:55:15 +0200

    fixed opacity problem

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var headertop = document.getElementsByClassName("top")[0];
+var men = document.getElementsByClassName("men")[0];
+var women = document.getElementsByClassName("women")[0];
 
 window.addEventListener("scroll", function(){
   console.log(this.scrollY);
@@ -8,6 +10,17 @@ window.addEventListener("scroll", function(){
   else{
     headertop.classList.remove("hide");
   }
-
+  if (this.scrollY >= 410){
+    for(let i = 0; i < men.children.length; i++){
+      women.children[i].style.opacity = 1.0;
+      men.children[i].style.opacity = 1.0;
+    }
+  }
+  else {
+    for(let i = 0; i < men.children.length; i++){
+      women.children[i].style.opacity = 0.0;
+      men.children[i].style.opacity = 0.0;
+    }
+  }
   
 });
